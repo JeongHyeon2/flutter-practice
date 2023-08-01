7bb6693ca7292ca142b50e10b9f7b952c3a7eea9
JeongHyeon2 <81457699+JeongHyeon2@users.noreply.github.com>
2023-08-01 09:27:33 +0900

    test

---
+++ inflearn_practice/lib/screen/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inflearn_practice/test/default_layout.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultLayout();
+  }
+}
